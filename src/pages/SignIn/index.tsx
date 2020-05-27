f8906c9e6de4e3542ca9855fe2ddca380b47944c
Renan Portugal do Rio <renanrpdrpro@gmail.com>
2020-05-27 11:25:12 -0300

    Done: Página de login.

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -10,22 +10,23 @@ const SignIn: React.FC = () => (
   <Container>
     <Content>
       <img src={logoImg} alt="GoBarber" />
+
       <form>
         <h1>Faça seu logon</h1>
 
-        <input placeholder="Email" />
+        <input placeholder="E-mail" />
 
         <input type="password" placeholder="Senha" />
 
         <button type="submit">Entrar</button>
 
         <a href="forgot">Esqueci minha senha</a>
-
-        <a href="login">
-          <FiLogIn />
-          Criar conta
-        </a>
       </form>
+
+      <a href="login ">
+        <FiLogIn />
+        Criar conta
+      </a>
     </Content>
 
     <Background />
